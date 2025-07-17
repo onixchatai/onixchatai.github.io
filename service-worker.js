6969faf1b6bbb2b098636f6fe99bb8ddfaff7525
Vikstagram <onixchatai@gmail.com>
2025-07-17 16:37:20 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-// Placeholder content for service-worker.js
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('onixchatai-cache-v1').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'services.html',
+        'manifest.json',
+        'icon.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
